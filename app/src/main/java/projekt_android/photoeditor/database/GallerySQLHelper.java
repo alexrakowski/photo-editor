74cf60c4330be1f24a18f8ed8a13adb44e0843c7
Piotr Izert <piotrizert@gmail.com>
2014-12-11 00:51:32 +0100

    implemented database helper for gallery images

--- app/src/main/java/projekt_android/photoeditor/database/GallerySQLHelper.java
+++ app/src/main/java/projekt_android/photoeditor/database/GallerySQLHelper.java
@@ -4,21 +4,23 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import projekt_android.photoeditor.Gallery;
+
 /**
  * Created by Piotrek on 2014-12-10.
  */
-public class ContentSQLHelper extends ImageSQLHelper {
+public class GallerySQLHelper extends ImageSQLHelper {
 
-    private static final String DATABASE_NAME = "content.db";
+    private static final String DATABASE_NAME = "gallery.db";
     private static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_CONTENT = "content";
+    public static final String TABLE_GALLERY = "gallery";
     public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_CONTENT_LINK = "contentlink";
+    public static final String COLUMN_GALLERY_LINK = "gallerylink";
 
-    private static final String CREATE = String.format("create table %s (%s integer primary key autoincrement, %s text not null);", TABLE_CONTENT, COLUMN_ID, COLUMN_CONTENT_LINK);
+    private static final String CREATE = String.format("create table %s (%s integer primary key autoincrement, %s text not null);", TABLE_GALLERY, COLUMN_ID, COLUMN_GALLERY_LINK);
 
-    public ContentSQLHelper(Context context) {
+    public GallerySQLHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -30,18 +32,19 @@ public class ContentSQLHelper extends ImageSQLHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
         Log.i(ContentSQLHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTENT);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_GALLERY
+        );
         onCreate(sqLiteDatabase);
     }
 
     @Override
     public String getTableName() {
-        return TABLE_CONTENT;
+        return TABLE_GALLERY;
     }
 
     @Override
     public String getImageColumnName() {
-        return COLUMN_CONTENT_LINK;
+        return COLUMN_GALLERY_LINK;
     }
 
     @Override
